78c0917ee5ebda4f9e7bd9fb54df22a776fd79c9
Kenneth Doerflein <ktd500@verizon.net>
2025-05-19 03:38:02 -0400

    update build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
+  basePath: "/Tic-Tac-Toe-React",
+  assetPrefix: "/Tic-Tac-Toe-React/",
 };
 
 export default nextConfig;
